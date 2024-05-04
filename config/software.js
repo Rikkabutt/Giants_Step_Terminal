cd21081004d5680f1161ba0f7b0cbcca6552a6c7
Rikkabutt <168696578+Rikkabutt@users.noreply.github.com>
2024-05-04 18:55:10 -0500

    Update software.js

--- config/software.js
+++ config/software.js
@@ -21,7 +21,7 @@ function rot13( s ) { // cf. https://en.wikipedia.org/wiki/ROT13
 }
 
 function selfdestruct( args ) {
-    const promptMsg = ["Are you sure you want to proceed with this action?(CURRENTLY NOT WORKING. IF YOU HAVE AN IDEA AS TO WHY NOT PLEASE LET THE CODER KNOW c:) (Y/N)"];
+    const promptMsg = ["Command Currently Non-Functional. Only No works. (Y/N)"];
     return { 
        if (answer.toUpperCase() === 'Y') {
                 // User confirmed, proceed with self-destruct sequence
